97cffbb2ef507b187ce52e651b3d2cbe8a990562
Bayu Yasaputro <bayuwy@gmail.com>
2022-08-02 20:15:43 +0700

    Initial Commit

---
+++ Siniar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Siniar
+//
+//  Created by Bayu Yasaputro on 02/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
